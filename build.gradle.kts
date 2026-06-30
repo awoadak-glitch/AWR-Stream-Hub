f1cb953b7445ac9f045ac8039f55df71ee125a4e
awoadak-glitch <awoadak@gmail.com>
2026-06-30 07:56:20 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file
 plugins {
     id("com.android.application") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false      // تم التحديث إلى 2.0.0
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false  // تم التحديث ليتوافق مع 2.0.0
 }
