bbf8729663a75867765315b33ab8902bbd27a934
awoadak-glitch <awoadak@gmail.com>
2026-06-30 08:06:11 -0700

    Update FavoritesScreen.kt

--- app/src/main/java/com/awr/streamhub/ui/screens/FavoritesScreen.kt
+++ app/src/main/java/com/awr/streamhub/ui/screens/FavoritesScreen.kt
@@ -65,7 +65,7 @@ fun FavoritesScreen(
         } else {
             LazyVerticalGrid(
                 columns = GridCells.Fixed(3),
-                contentPadding = PaddingValues(horizontal = 14.dp, vertical = 4.dp, bottom = 90.dp),
+                contentPadding = PaddingValues(start = 14.dp, top = 4.dp, end = 14.dp, bottom = 90.dp),
                 horizontalArrangement = Arrangement.spacedBy(10.dp),
                 verticalArrangement = Arrangement.spacedBy(10.dp)
             ) {
